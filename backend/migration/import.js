b6e2b36f327fd47a9830e732e342840fbb8f5d13
Sergey Dvoynikov <svdvoynikov@gmail.com>
2015-09-24 17:12:34 +0500

    Migration utils

--- backend/migration/import.js
+++ backend/migration/import.js
@@ -1,17 +1,17 @@
-var fs = require("fs");
 var db = require("../db");
 
-fs.readFile("themes.json", function(err, themes) {
-    var processed = 0;
-    themes = JSON.parse(themes);
-    db.connect(process.env.MONGO_URL, function() {
-        themes.forEach(data => {
-            var theme = new db.models.Theme(data);
-            theme.save(function() {
-                processed++;
-                if (processed == themes.length) {
-                    process.exit();
-                }
+db.connect(process.env.SOURCE_DB, function() {
+    db.models.Theme.find({}).exec(function(err, themes) {
+        var processed = 0;
+        db.connect(process.env.MONGO_URL, function() {
+            themes.forEach(data => {
+                var theme = new db.models.Theme(data);
+                theme.save(function() {
+                    processed++;
+                    if (processed == themes.length) {
+                        process.exit();
+                    }
+                });
             });
         });
     });
