0551e029e6b9685ee527d3cddeca2f533a04be3d
Sergey Dvoynikov <svdvoynikov@gmail.com>
2015-10-31 13:36:53 +0500

    Object.assign for IE

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -13,12 +13,19 @@ import BuildArchiveView from "views/BuildArchiveView/BuildArchiveView";
 
 import "./main.less";
 
+// Google Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
 ga('create', 'UA-44640459-2', 'auto');
+
+//IE support
+if (!Object.assign) {
+    Object.assign = function(object, ...params) {
+        return params.reduce((o, p) => (p && Object.keys(p).forEach(k => o[k] = p[k]) && o) || o, object);
+    }
+}
 
 Request.setApiFn(apiFn);
 
