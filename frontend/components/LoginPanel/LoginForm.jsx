91c830b81e5b94ac655fdceff5ba8082dff00c08
Sergey Dvoynikov <svdvoynikov@gmail.com>
2015-11-04 12:03:47 +0500

    The Enter button does not work for Log In and Register #13

--- frontend/components/LoginPanel/LoginForm.jsx
+++ frontend/components/LoginPanel/LoginForm.jsx
@@ -42,31 +42,33 @@ export default class LoginForm extends Component {
 
     render() {
         return (
-            <ul className={"popup-menu " +
+            <form>
+                <ul className={"popup-menu " +
                  ((this.props.type === LoginFormType.TYPE_LOGIN) ? "login" : "register")}>
-                {(this.props.type === LoginFormType.TYPE_LOGIN) ? [] :
+                    {(this.props.type === LoginFormType.TYPE_LOGIN) ? [] :
                     <li>
                         <label htmlFor="loginName">{i18n("Name (optional)")}</label>
                         <input type="text" value={this.props.name} onChange={this.onFieldChange}
                                ref="loginName" id="loginName"/>
                     </li>
-                    }
-                <li>
-                    <label htmlFor="loginEmail">{i18n("Email")}</label>
-                    <input type="email" value={this.props.email} onChange={this.onFieldChange}
-                           ref="loginEmail" id="loginEmail"/>
-                </li>
-                <li>
-                    <label htmlFor="loginPassword">{i18n("Password")}</label>
-                    <input type="password" value={this.props.password}
-                           onChange={this.onFieldChange}
-                           ref="loginPassword" id="loginPassword"/>
-                </li>
-                <li className="submit">
-                    <button onClick={this.onSubmit} type="submit">
-                        {(this.props.type === LoginFormType.TYPE_LOGIN) ? i18n("Log In") : i18n("Register")}
-                    </button>
-                </li>
-            </ul>);
+                        }
+                    <li>
+                        <label htmlFor="loginEmail">{i18n("Email")}</label>
+                        <input type="email" value={this.props.email} onChange={this.onFieldChange}
+                               ref="loginEmail" id="loginEmail"/>
+                    </li>
+                    <li>
+                        <label htmlFor="loginPassword">{i18n("Password")}</label>
+                        <input type="password" value={this.props.password}
+                               onChange={this.onFieldChange}
+                               ref="loginPassword" id="loginPassword"/>
+                    </li>
+                    <li className="submit">
+                        <button onClick={this.onSubmit} type="submit">
+                            {(this.props.type === LoginFormType.TYPE_LOGIN) ? i18n("Log In") : i18n("Register")}
+                        </button>
+                    </li>
+                </ul>
+            </form>);
     }
 }
