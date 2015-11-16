499b227e4ec6bd00899363366c2520c9c2be151c
Lena Dvoynikova <dvoynikova.lena@gmail.com>
2015-11-16 21:31:38 +0500

    Add new test for opening pages

--- tests/frontend/index.js
+++ tests/frontend/index.js
@@ -8,5 +8,15 @@ module.exports = {
             .waitForElementVisible(".preview-list", 5000)
             .assert.containsText(".pager .current", "1")
             .end();
+    },
+    
+    "open next page": function(browser) {
+        browser
+            .url(browser.globals.url)
+            .waitForElementVisible(".pager a:nth-child(2)",5000)
+            .click(".pager a:nth-child(2)")
+            .waitForElementVisible(".preview-list", 5000)
+            .assert.containsText(".pager .current", "2")
+            .end();
     }
 };
