f97bc46704c740c48ddd49acda5451cb4c2cd832
Sergey Dvoynikov <svdvoynikov@gmail.com>
2016-01-10 10:52:35 +0500

    AD fix

--- frontend/components/PreviewList/PreviewList.jsx
+++ frontend/components/PreviewList/PreviewList.jsx
@@ -4,22 +4,20 @@ import * as Types from "../PropTypes";
 
 require("./PreviewList.less");
 
-var ad = `<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- color-themes.com/list-item -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:336px;height:280px"
-     data-ad-client="ca-pub-8538465790539239"
-     data-ad-slot="6773128704"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>`;
-
 var adCoefficient = 15;
 
 export default class PreviewList extends Component {
     static propTypes = {
         layout: React.PropTypes.object.isRequired,
         themes: PropTypes.arrayOf(Types.theme).isRequired
+    }
+
+    componentDidMount() {
+        var adCount = Math.ceil(this.props.themes.length / adCoefficient);
+        adsbygoogle = window.adsbygoogle || [];
+        for (var i = 0; i < adCount; i++) {
+            adsbygoogle.push({});
+        }
     }
 
     render() {
@@ -33,7 +31,12 @@ export default class PreviewList extends Component {
         return (
             <div className="preview-list" ref="list">
                 {prepared.map(theme => theme.ad ?
-                    <div className="ad" dangerouslySetInnerHTML={{__html: ad}}></div> :
+                    <div className="ad">
+                        <ins className="adsbygoogle"
+                             style={{display:"inline-block",width:"336px",height:"280px"}}
+                             data-ad-client="ca-pub-8538465790539239"
+                             data-ad-slot="6773128704"></ins>
+                    </div> :
                     <PreviewListItem theme={theme} layout={this.props.layout} key={theme._id}/>)}
             </div>);
     }
