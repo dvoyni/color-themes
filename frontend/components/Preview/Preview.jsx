1b0c25ad4297d7edd7c686467d9ab64c72bf011d
Sergey Dvoynikov <svdvoynikov@gmail.com>
2017-06-21 16:42:07 +0500

    Inheritance support was added to themes

--- frontend/components/Preview/Preview.jsx
+++ frontend/components/Preview/Preview.jsx
@@ -14,9 +14,12 @@ export default class Preview extends Component {
         var html = "";
         if (styles && layout) {
             html = layout.text.replace(layout.tagStart, function(match, value) {
-                var style = styles[value];
-                if (style) {
-                    return "<span style=\"" + style.css + "\">";
+                var values = value.split(layout.tagSeparator);
+                for (var i = 0; i < values.length; i++) {
+                    var style = styles[values[i]];
+                    if (style) {
+                        return "<span style=\"" + style.css + "\">";
+                    }
                 }
                 return "<span>";
             }).replace(layout.tagEnd, "</span>");
